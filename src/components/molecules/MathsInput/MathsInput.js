ae79274d12ec410721de7127b672694ca9f9e1a8
Richard Ng <richardcrng@gmail.com>
2019-07-23 09:54:50 +0100

    make MathsInput set itself as active by default onClick

--- src/components/molecules/MathsInput/MathsInput.js
+++ src/components/molecules/MathsInput/MathsInput.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import useMathQuillMathField from '../../../helpers/use-mathquill/useMathQuill/MathField';
+import { useSetActiveHtmlElement } from '../../organism/MathsEnabler';
 
-function MathsInput({ id, onBlur, onClick, onFocus, style, ...rest }, ref) {
+function MathsInput({ id, onBlur, onClick, onFocus, setActiveOnClick = true, style, ...rest }, ref) {
   useMathQuillMathField(ref)
+  const setActive = useSetActiveHtmlElement()
 
   return (
     <span
       ref={ref}
-      {...{ id, onBlur, onClick, onFocus, style }}
+      onClick={(e) => {
+        if (setActiveOnClick) setActive(ref.current)
+        if (typeof onClick === 'function') onClick(e)
+      }}
+      {...{ id, onBlur, onFocus, style }}
       {...rest}
     />
   )
